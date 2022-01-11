586f861e3c1e8385d553a1cc8bbec636d4228741
Muhammad Daffa ZW <56968825+admiralkuznetsov45@users.noreply.github.com>
2022-01-11 11:15:40 +0700

    Update Migrate User
    Update Migrate User

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -3,8 +3,10 @@ package migrate
 import (
 	"SistemVaksinAPI/config"
 	m_admin "SistemVaksinAPI/features/admin/data"
+	m_user  "SistemVaksinAPI/features/user/data"
 )
 
 func AutoMigrate() {
 	config.DB.AutoMigrate(&m_admin.Admin{})
+	config.DB.AutoMigrate(&m_user.User{})
 }
