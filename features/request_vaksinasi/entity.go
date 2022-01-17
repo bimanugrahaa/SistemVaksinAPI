9977b7d7b59c68c73062bc731fbeda34cdc24830
Muhammad Daffa ZW <andafsul4@gmail.com>
2022-01-17 17:06:56 +0700

    Upgrade  Request Vaksin
    Request Vaksin

--- features/request_vaksinasi/entity.go
+++ features/request_vaksinasi/entity.go
@@ -1,1 +1,32 @@
 package requestvaksinasi
+
+type RequestvaksinCore struct {
+	ID              int
+	Jenisvaksin     string
+	Dosis_pertama   bool
+	Dosis_kedua     bool
+	Status_satu     string
+	Status_dua      string
+	Nama            string
+	NIK             int
+	Jam_pertama     string
+	Jam_Kedua       string
+	Tanggal_pertama string
+	Tanggal_kedua   string
+	Provinsi        string
+	Kota            string
+	Kelurahan       string
+	Kecamatan       string
+}
+
+type Bussiness interface {
+	CreateRequestvaksin(data RequestvaksinCore) (resp RequestvaksinCore, err error)
+	GetAllRequestvaksin() (resp []RequestvaksinCore)
+	GetRequestvaksinByID(id int) (resp RequestvaksinCore, err error)
+}
+
+type Data interface {
+	InsertRequestvaksin(data RequestvaksinCore) (resp RequestvaksinCore, err error)
+	SelectAllRequestvaksin() (resp []RequestvaksinCore)
+	SelectRequestvaksinByID(id int) (resp RequestvaksinCore, err error)
+}
